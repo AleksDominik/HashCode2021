fb0ba53ecff463f2fadfa1ddaef9c948a2a58b25
Aleks <aleksnougbele@gmail.com>
2021-02-25 20:56:03 +0000

    init

---
+++ Hashcode.py
@@ -0,0 +1,70 @@
+
+class Car:
+    def __init__(self, Route):
+        self.Route=Route    #routeactuelle
+        Time_end= None # initialisé a la fin
+        path=List(Route)
+        Status='termine', 'start','onroute'
+        time_spend_on_the_road=0 
+    def avancer(self):
+        if self.Status='start' and self.Route.:
+            self.Route.Queue
+
+
+
+    
+
+
+class Route:
+    def __init(self, K):
+        self.Longueur=K  #given
+
+
+class Intersection :
+    income=List(Route)
+    outcome=List(Route)
+    schedule= List[int]
+
+class Reseau:
+    def __init__(self, tableau_des_route, tableau_des_intersections,T):    
+        self.tableau_des_route=tableau_des_route
+        self.tableau_des_intersections= tableau_des_intersections
+        self.Temps_de_simulation=T
+
+
+    def initialisation():
+     pass 
+    def  simuler():
+    """
+    performance du reseau
+    """
+        for indice in range(self.Temps_de_simulation-1):
+            for cars in self.tableau_des_voitures:
+                cars.avancer()
+
+
+
+
+
+
+    def mutation(self ):
+     pass
+    
+    def reproduction(self, Reseau ):
+     pass
+
+if  name=='__main__':
+
+
+nombre_de_generation=n 
+nombre_de_population=M
+List<Reseau> Population=[Reseau()  for k in range(nombre_de_generation)]
+
+for  indice in range(nombre_de_generation):
+
+
+
+
+
+
+
