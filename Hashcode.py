bb588e491bfcdbe75217e35226fe70836d48b4c7
Aleks <aleksnougbele@gmail.com>
2021-03-07 07:44:56 +0000

    feature end simulation

--- Hashcode.py
+++ Hashcode.py
@@ -1,24 +1,32 @@
 import queue
 import typing
+from itertools import accumulate
+import math, random
 
 class Car:
+    """
+    Car class
+    """
     def __init__(self, Route, priority, status, path):
-        self.Route=Route    #routeactuelle
+        self.Route=Route    #routeactuelle( only the name)
         self.Time_end= 0 # initialisé a la fin
-        self.path=path
-        self.status= status#'termine', 'start','onroute'
-        self.time_spend_on_the_road=0 
+        self.path=path # array of name of route
+        self.status= status#'termine', 'start','onroute','onqueue'
+        self.time_spend_on_the_road=0
         self.priority=priority
-    def avancer(self):
-        if self.Status=='start': #and self.Route.intersection.schedule[self.Route.name]:
-            #self.Route.queue
-            pass
+        self.path_queue=queue.Queue()
+        for path_ in self.path:
+            self.path_queue.put(path_)
+
     def display(self):
-        print(f' This a car with the actual route has given { self.Route} the time of  arrival {self.Time_end}, the path {self.path}, the status {self.status} the priority {self.priority} ')
+        print(f' This is a car with the actual route has given { self.Route} the time of  arrival {self.Time_end}, the path {self.path}, the status {self.status} the priority {self.priority}, the time spend on route is {self.time_spend_on_the_road}')
 
 class Route:
-    def __init__(self, K,name, intersection) : 
-        self.Longueur=K  #given
+    """
+    Route classe
+    """
+    def __init__(self, K,name, intersection) :
+        self.Longueur=int(K)  #given
         self.queue= queue.Queue()
         self.name=name
         self.intersection=intersection # intersection where route end
@@ -28,13 +36,54 @@ class Route:
 
 
 class Intersection :
-    def __init__(self,income, outcome, schedule):
+    """
+    Class for intersection
+
+    """
+    def __init__(self,income, outcome, schedule, index_of_intersection):
         self.income=income
         self.outcome=outcome
         self.schedule=schedule  #((name, int))
-        self.internal_time=0
-    def which_road_is_green():
-        pass
+        self.index_time= list(accumulate([k[1] for k in self.schedule]))
+        # print(sum( [[k[1] for k in self.schedule]]))
+        self.sum_time=sum(  [int(k[1]) for k in self.schedule] )
+        self.internal_time=0 #
+        self.index_of_intersection=index_of_intersection
+        self.can_go_through=True
+
+    def initialize_schedule(self,top_time):
+        """
+        initialize self.schedule to a list of list
+        (
+            (name of the street, temp of green ligth)
+        )
+        the order matter
+        # TODO in mutation think at a swaping operation to change the order of the street in the schedule.
+        """
+        for name in [k.name for k in self.income]:
+            self.schedule.append((name, random.randint(1,top_time)))
+        print(f' the schedule is {self.schedule} ')
+        self.sum_time=sum(  [int(k[1]) for k in self.schedule] )
+        self.index_time= list(accumulate([k[1] for k in self.schedule]))
+
+
+
+    def which_road_is_green(self,TIME):
+        """
+        function that manage the schedule for infor management
+        """
+        mod= TIME%self.sum_time
+        # print(self.sum_time)
+        # print(f'the modulo is {mod}')
+        # print(self.index_time)
+        # print({f'the index time is {self.index_time}'})
+        for k in range(len(self.index_time)):
+            if mod <self.index_time[0]:
+                return self.schedule[0]
+
+            elif self.index_time[k]<=mod<self.index_time[k+1]:
+                return self.schedule[k+1]
+
     def display(self):
         print('Intersection: the routes in the income are')
         for route in self.income:
@@ -42,73 +91,143 @@ class Intersection :
         print('Intersection: the routes in the outcome are')
         for route in self.outcome:
             route.display()
+        print( "the schedule is ")
+        print(self.schedule)
 
 class Reseau:
-    def __init__(self, tableau_des_route, tableau_des_intersections,T):
+    """
+    Network classes handle the env for simulation 
+    """
+    def __init__(self,file):
+        f=file
+
+        lines= f.readlines()
+        # print(lines)
+        lines=[k.replace('\n', '').split(' ') for k in lines]
+        first_line=lines[0]
+        time_of_simulation,numbers_of_intersection, numbers_of_streets, numbers_of_cars, D=[int(k) for k in first_line]
+
+        # print(time_of_simulation,numbers_of_intersection, numbers_of_streets, numbers_of_cars, D )
+
+        list_intersection= [ Intersection( [],[],[], i) for i in range(numbers_of_intersection)]
+        table_of_routes={}
+        list_of_cars= []
+
+        for index_inter, line in enumerate(lines[1:numbers_of_streets+1]):
+            """
+            initialise the route and intersection
+            """
+            # print(line)
+            table_of_routes[line[2]]=Route( line[-1], line[2],list_intersection[int(line[1])],)
+            list_intersection[ int(line[1])  ].outcome.append( table_of_routes[line[2]])
+            list_intersection[int(line[1])].income.append(table_of_routes[line[2]])
 
 
-        self.tableau_des_route=tableau_des_route
-        self.tableau_des_intersections= tableau_des_intersections
-        self.Temps_de_simulation=T
+
+        #TODO initialize schedule
+
+        for intersection in list_intersection:
+            intersection.initialize_schedule(time_of_simulation/2)
+            intersection.display()
+
+        for priority, line in enumerate(lines[numbers_of_streets+1:]):
+            list_of_cars.append(Car(line[1],priority, 'start', line[1:]))
+        # print( list_of_cars)
+
+        self.tableau_des_routes=table_of_routes
+        self.tableau_des_intersections= list_intersection
+        self.Temps_de_simulation=time_of_simulation
+        self.tableau_des_cars=list_of_cars
+        self.D=D
+        self.Gain=int(first_line[-1])
+
+    def __forwad_car__(self,Car_:Car, TIME):
+        # print('dddkdkd')
+        print(self.tableau_des_routes[Car_.Route].intersection.which_road_is_green(TIME))
+        is_green= self.tableau_des_routes[Car_.Route].intersection.which_road_is_green(TIME)[0]==Car_.Route
+        Car_.time_spend_on_the_road+=1
+        can_go_through= self.tableau_des_routes[Car_.Route].intersection.can_go_through==True
+        if Car_.status=='start'   and is_green and  can_go_through:
+            print("voiture en position")
+            Car_.Route=Car_.path_queue.get(False)#the next one
+            self.tableau_des_routes[Car_.Route].intersection.can_go_through=False
+            Car_.status='onroute'
+        elif Car_.status=="onroute":
+            print('car on track')
+            # print('erererere')
+            # print(Car_.time_spend_on_the_road)
+            # print(self.tableau_des_routes[Car_.Route].Longueur)
+            if Car_.time_spend_on_the_road>=self.tableau_des_routes[Car_.Route].Longueur:
+                if Car_.path_queue.empty():
+                    Car_.status='termine'
+                    Car_.Time_end=TIME
+                else:
+                    #TODO:end with queue empty and status onqueue
+                    Car_.status='onqueue'
+
+        elif Car_.status=='onqueue' and is_green and can_go_through:
+            print('car waiting')
+            try:
+                 Car_.Route=Car_.path_queue.get(False)
+                 self.tableau_des_routes[Car_.Route].intersection.can_go_through=False
+                 Car_.status='onroute'
+            except:
+                print('car arrived finally ///////////////////')
+                Car_.status='termine'
+                Car_.Time_end=TIME
+        else:
+            print('nothing happen to this car')
+
+    def __score_car__(self,Car):
+        """
+        Compute the gained score of a car
+        """
+        if Car.status=='termine':
+            return  self.Gain +(self.D -Car.Time_end)
+        else:
+            return 0
+        
+
 
 
     def initialisation():
-     pass 
-    def  simuler():
+     pass
+    def simuler(self):
         """
         performance du reseau
         """
-        for indice in range(self.Temps_de_simulation-1):
-            for cars in self.tableau_des_voitures:
-                cars.avancer()
-            
+        for time in range(self.Temps_de_simulation-1):
+            for inter in self.tableau_des_intersections:
+                inter.can_go_through=True
+
+            for car in self.tableau_des_cars:
+                print("avancement de la voiture")
+                # car.display()
+                self.__forwad_car__(car, time)
+        
+        for car in self.tableau_des_cars:
+            print('eeeee')
+            print(list(car.path_queue.queue))
+            car.display()
+        self.points= sum([self.__score_car__(car) for car in self.tableau_des_cars])
+        print(f'the points of this simulation are {self.points}')
+
 
 
     def mutation(self ):
+        
      pass
-    
+
     def reproduction(self, Reseau ):
      pass
 
 if  __name__=='__main__':
+    R=Reseau(open('a.txt', 'r'))
 
-    f=open('a.txt', 'r')
-    lines= f.readlines()
-    lines=[k.replace('\n', '').split(' ') for k in lines]
-    first_line=lines[0]
-    time_of_simulation,numbers_of_intersection, numbers_of_streets, numbers_of_cars, D=[int(k) for k in first_line]
-    print(time_of_simulation,numbers_of_intersection, numbers_of_streets, numbers_of_cars, D )
-
-
-    list_intersection= [ Intersection( [],[],{}) for _ in range(numbers_of_intersection)]
-    list_of_routes=[]
-    list_of_cars= []
-
-    for line in lines[1:numbers_of_streets+1]:
-        """
-        initialise the route and intersection
-        """
-        #print(line)
-        # print(line)
-        list_of_routes.append(Route( line[-1], line[2],list_intersection[int(line[1])]) ) 
-        list_intersection[ int(line[0])].outcome.append( list_of_routes[-1])
-        list_intersection[int(line[1])].income.append(list_of_routes[-1])
-
-    for index, intersection in enumerate( list_intersection):
-        print(index)
-        intersection.display()
-    for priority, line in enumerate(lines[numbers_of_streets+1:]):
-        list_of_cars= [ Car(line[0],priority, 'start', line[1:])]
-    for car in list_of_cars:
-        car.display()
-    print(list_intersection, list_of_routes)
     nombre_de_generation=2
     nombre_de_population=4
-    # Population=[Reseau()  for k in range(nombre_de_generation)]
+    R.simuler()
 
-    for  indice in range(nombre_de_generation):
-        pass
-    
 
 
 
